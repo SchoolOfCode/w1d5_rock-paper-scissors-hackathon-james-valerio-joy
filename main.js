97c5b7ec4c25cb4568471f861204c02fb6436f4b
perrettJ4 <perrettj4@gmail.com>
2021-07-30 11:15:16 +0100

    task 6

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 function getWinner(playerMove, computerMove) {
     if (playerMove === computerMove) {
         console.log('Tie');
+        ties += 1;
         return "0";
     }
     else if (playerMove === 'rock' && computerMove === 'paper' ||
             playerMove === 'paper' && computerMove === 'scissors' || playerMove === 'scissors' && computerMove === 'rock') {
         console.log('The computer wins');
+        losses += 1;
         return "-1";
     }
     else {
         console.log('The player wins');
+        wins += 1
         return "1";
     }
 }
@@ -17,6 +20,11 @@ function getWinner(playerMove, computerMove) {
 let isConfirmed = true
 let exit = false
 let result = 0
+let wins = 0
+let losses = 0
+let ties = 0
+
+
 
 while (isConfirmed === true) {
     let valid = false;
@@ -35,8 +43,13 @@ while (isConfirmed === true) {
 
     let computerMove = ['rock', 'paper', 'scissors'][Math.floor(Math.random() * 3)];
     result = Number(getWinner(playerMove, computerMove))+result;
-    alert(`The player move is: ${playerMove}.
-    The computer move is: ${computerMove}.
-    Player one Score: ${result}!`);
-    isConfirmed = confirm("Do you want to continue playing?");
+    isConfirmed = confirm((`The player move is: ${playerMove}.
+The computer move is: ${computerMove}.
+Player one Total Score: ${result}!
+Total wins: ${wins}
+Total losses: ${losses}
+Total ties: ${ties}
+
+Do you want to continue playing?`)
+    );
 }
