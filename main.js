d92ee3496b2138df83803dd672895cc9d917f409
tzuyunlin <tzuyunlin.tw@gmail.com>
2021-07-30 11:01:52 +0100

    task5

--- main.js
+++ main.js
@@ -1,18 +1,10 @@
-let valid = false;
-while(valid === false) {
-    var playerMove = prompt("Please enter rock, paper or scissors:");
-    if(playerMove === "rock" || playerMove === "paper" || playerMove === "scissors") {
-        valid = true;
-    }
-}
-
 function getWinner(playerMove, computerMove) {
     if (playerMove === computerMove) {
         console.log('Tie');
         return "0";
     }
     else if (playerMove === 'rock' && computerMove === 'paper' ||
-            playerMove === 'paper' && computerMove === 'scissor' || playerMove === 'scissor' && computerMove === 'rock') {
+            playerMove === 'paper' && computerMove === 'scissors' || playerMove === 'scissors' && computerMove === 'rock') {
         console.log('The computer wins');
         return "-1";
     }
@@ -22,8 +14,29 @@ function getWinner(playerMove, computerMove) {
     }
 }
 
-let computerMove = ['rock', 'paper', 'scissor'][Math.floor(Math.random() * 3)];
-let result = getWinner(playerMove, computerMove);
-alert(`The player move is: ${playerMove}.
-The computer move is: ${computerMove}.
-Player one Score: ${result}!`);
+let isConfirmed = true
+let exit = false
+let result = 0
+
+while (isConfirmed === true) {
+    let valid = false;
+    while(valid === false) {
+        var playerMove = prompt("Please enter rock, paper or scissors:");
+        if(playerMove === "rock" || playerMove === "paper" || playerMove === "scissors") {
+            valid = true;
+        }
+        else if (playerMove === null){
+            exit = true;
+            valid = true;
+        }
+    }
+        if (exit === true){
+        break;}
+
+    let computerMove = ['rock', 'paper', 'scissors'][Math.floor(Math.random() * 3)];
+    result = Number(getWinner(playerMove, computerMove))+result;
+    alert(`The player move is: ${playerMove}.
+    The computer move is: ${computerMove}.
+    Player one Score: ${result}!`);
+    isConfirmed = confirm("Do you want to continue playing?");
+}
